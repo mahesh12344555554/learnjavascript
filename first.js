40c9db29d28a94b0e8c6a131bb72d95e333665e0
mac <mac@macs-MBP.lan>
2024-07-11 08:54:19 +0545

    added scoping

---
+++ first.js
@@ -0,0 +1,30 @@
+// const product = {
+
+//     name : " ball pen",
+//     rating : 4,
+//     offer:5,
+//     price: 350,
+// };
+// // console.log(product);
+
+// const arr =[3,4,5,6,7,8];
+// console.log(arr);
+// const date = new date();
+// console.log(type of date);
+
+// let x=  "";
+// let  result =  boolean(x);
+// console.log(result);
+// // console.log(typof  result);
+
+// string method
+// const result="hello world"
+
+// // length
+// console.log(result.length)
+
+// const result =" hello world";
+// const output = " i love javascript";
+// const str3 = " ma gardina rey";
+
+// console.log(result +" " + output + " " + str3);
